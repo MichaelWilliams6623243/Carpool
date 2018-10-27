399a4157613880cf52150775916115f74d571cd3
MichaelWilliams6623243 <44508799+MichaelWilliams6623243@users.noreply.github.com>
2018-10-27 00:42:21 +0000

    Add files via upload

---
+++ commuter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python36
+
+import subprocess,os,cgi
+
+print("content-type: text/html")
+
+cmd=cgi.FieldStorage()
+
+customer=cmd.getvalue('customer')
+name=cmd.getvalue('name')
+email=cmd.getvalue('email')
+psw=cmd.getvalue('psw')
+psw_repeat=cmd.getvalue('psw-repeat')
+
+if psw!=psw_repeat:
+	print("\n")
+	print("""
+		Passwords do not match
+		<br /><a href='register.html'>go back</a>
+		""")
+elif customer=="commuter":
+	print("location: commuter.html?name="+name+"?email="+email+"?psw="+psw)
+	print("\n")
+elif customer=="owner":
+	print("location: owner.html?name="+name+"?email="+email+"?psw="+psw)
+	print("\n")
